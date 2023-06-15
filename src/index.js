66cbe898f24cd0c03ed9a728d997caaaf84271e2
Disha <bidisha2125@gmail.com>
2023-06-15 23:42:16 +0530

    added sequelize cli

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const {PORT} = require('./config/serverConfig');
-const bodyParser = require("body-parser");
+const { PORT } = require('./config/serverConfig');
+
 
 const setupAndStartServer = async ()  => {
 
     // create the express object
     const app = express();
 
-    app.use(bodyparser.json());
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
 
