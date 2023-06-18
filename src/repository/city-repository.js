04912fc25e79e8eef0eb01e30471943c66834256
Disha <bidisha2125@gmail.com>
2023-06-18 14:31:33 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,19 +6,46 @@ class CityRepository {
             const city = await City.create({ name });
             return city;
         } catch (error) {
+            console.log("Something went wrong");
             throw {error};
         }
     }
 
-    async ddeleteCity(cityId) {
+    async deleteCity(cityId) {
         try {
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong");
             throw {error};
+        }
+    }
+
+    async updateCity(){
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            throw { error };
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            throw { error };
         }
     }
 }
